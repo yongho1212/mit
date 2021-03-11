85397b715d0fb55694dc1a8a039d133997f39f3b
yongho6800@gmail.com <yongho6800@gmail.com>
2021-03-11 16:51:51 +0900

    added centerpin/changed to class,render()

--- Screens/Main/index.js
+++ Screens/Main/index.js
@@ -1,28 +1,45 @@
-import React from 'react';
-import {Text, View, Pressable} from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, Pressable, Image} from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; 
 import styles from './styles'
 
 import {useNavigation} from '@react-navigation/native';
 
+const latitudeDelta = 0.015
+const longitudeDelta = 0.0121
 
-const Main = (props) => {
-  const navigation = useNavigation();
 
+export default class Main extends Component{
+
+  state = {
+    region: {
+      latitudeDelta,
+      longitudeDelta,
+      latitude: 37.36169446014372,
+      longitude: 127.119963242058,
+    }
+  };
+
+  onChangeValue = region => {
+    this.setState({
+      region
+    });
+  };
+
+    render(){
     return(
+      
         <View style={{width: '100%', height: '100%'}}>
           
         <MapView
           style={{width: '100%', height: '100%'}}
           provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-
-          region={{
-            latitude: 37.36169446014372,
-            longitude: 127.119963242058,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
+          initialRegion={this.state.region}
+          onRegionChangeComplete = {this.onChangeValue}
+          
+          
         >
+        
           <View>
             <Pressable 
             style={styles.locationSearchBtn}
@@ -33,9 +50,10 @@ const Main = (props) => {
             </Pressable>
           </View>
         </MapView>
+        <View style={{top:'50%', left:'50%', marginLeft:-24, marginTop:-48, position:'absolute'}}>
+          <Image style={{height:30, width:30}} source={{uri:'https://cdn2.iconfinder.com/data/icons/interface-elements-2/512/pin_map_check_pins-512.png'}} />
+        </View>
       </View>
     );
-};
-
-
-export default Main;
+}
+}

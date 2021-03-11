75562eae4f4c7250b99a402887b26491e61016e1
yongho6800@gmail.com <yongho6800@gmail.com>
2021-03-12 01:02:45 +0900

    changed Marker

--- Screens/Main/index.js
+++ Screens/Main/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import {Text, View, Pressable, Image} from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; 
+import {Text, View, Pressable, Image, Button} from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'; 
 import styles from './styles'
 
 import {useNavigation} from '@react-navigation/native';
@@ -10,7 +10,7 @@ const longitudeDelta = 0.0121
 
 
 export default class Main extends Component{
-
+  
   state = {
     region: {
       latitudeDelta,
@@ -25,8 +25,11 @@ export default class Main extends Component{
       region
     });
   };
-
+  
+  
     render(){
+      
+      
     return(
       
         <View style={{width: '100%', height: '100%'}}>
@@ -41,18 +44,15 @@ export default class Main extends Component{
         >
         
           <View>
-            <Pressable 
+            <Pressable
+            onPress={() => this.props.navigation.navigate('LocationSearch')}
             style={styles.locationSearchBtn}
-            onPress={() => navigation.navigate('LocationSearch')}
-            >
-              
-              <Text style={styles.locationSearchText}>Location info</Text>
-            </Pressable>
+            
+            />
           </View>
+          <Marker coordinate={this.state.region}   />
         </MapView>
-        <View style={{top:'50%', left:'50%', marginLeft:-24, marginTop:-48, position:'absolute'}}>
-          <Image style={{height:30, width:30}} source={{uri:'https://cdn2.iconfinder.com/data/icons/interface-elements-2/512/pin_map_check_pins-512.png'}} />
-        </View>
+
       </View>
     );
 }

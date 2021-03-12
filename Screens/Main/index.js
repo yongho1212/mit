df0ec381fb696e33e98be8b2ca0ee8b244e4df38
yongho6800@gmail.com <yongho6800@gmail.com>
2021-03-12 17:27:49 +0900

    location search connected to ong.lat

--- Screens/Main/index.js
+++ Screens/Main/index.js
@@ -17,15 +17,27 @@ export default class Main extends Component{
       longitudeDelta,
       latitude: 37.36169446014372,
       longitude: 127.119963242058,
-    }
+    },
+    parseLat: null,
+    parseLong: null,
   };
 
   onChangeValue = region => {
     this.setState({
       region
     });
+    console.warn(this.state.region);
+    var test = JSON.stringify(this.state.region);
+    console.log(JSON.parse(test).latitude);
+    this.regionChange(JSON.parse(test));
   };
   
+  regionChange = (data) => {
+    this.setState({
+      parseLong: data.latitude,
+      parseLat: data.longitude
+    })
+  }
   
     render(){
       
@@ -39,18 +51,21 @@ export default class Main extends Component{
           provider={PROVIDER_GOOGLE} // remove if not using Google Maps
           initialRegion={this.state.region}
           onRegionChangeComplete = {this.onChangeValue}
-          
-          
+          showsMyLocationButton = {true}
+          showsUserLocation = {true}
         >
         
           <View>
             <Pressable
             onPress={() => this.props.navigation.navigate('LocationSearch')}
             style={styles.locationSearchBtn}
-            
-            />
+            >
+              <Text>{this.state.parseLat}</Text>
+              <Text>{this.state.parseLong}</Text>
+            </Pressable>  
           </View>
-          <Marker coordinate={this.state.region}   />
+          <Marker coordinate={this.state.region}   /> 
+      {/* this.state.region의 값을 이용하여 현위치와 버튼 텍스트(geocoding) 연동? */}
         </MapView>
 
       </View>
